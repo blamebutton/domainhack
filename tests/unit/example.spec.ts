99e7e24b3ded29ff1e2ad52e95bd62347973407a
Bram Ceulemans <bramceulemans@me.com>
2020-06-10 22:42:47 +0200

    fixed import for example.spec.ts

--- tests/unit/example.spec.ts
+++ tests/unit/example.spec.ts
@@ -1,10 +1,10 @@
 import { shallowMount } from '@vue/test-utils';
-import HelloWorld from '@/components/HelloWorld.vue';
+import DomainInput from '@/components/DomainInput.vue';
 
-describe('HelloWorld.vue', () => {
+describe('DomainInput.vue', () => {
   it('renders props.msg when passed', () => {
     const msg = 'new message';
-    const wrapper = shallowMount(HelloWorld, {
+    const wrapper = shallowMount(DomainInput, {
       propsData: { msg },
     });
     expect(wrapper.text()).toMatch(msg);
